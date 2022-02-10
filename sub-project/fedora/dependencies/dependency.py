e907c8c52c326aabb2336f2926c6f6bd5af0d071
fschenkels <sfschenkel@gmail.com>
2022-02-10 11:46:06 -0300

    [fedora] adding volume

--- sub-project/fedora/dependencies/dependency.py
+++ sub-project/fedora/dependencies/dependency.py
@@ -1,7 +1,22 @@
+from cmath import log
 import requests
+import logging
+
+# FIXME: hardcoded, what a shame!
+LOG_FILE = "/home/jacare/log/dependency.log"
+
+logging.basicConfig(filename=LOG_FILE,
+                    filemode='a',
+                    format='%(asctime)s,%(msecs)d %(levelname)s %(message)s',
+                    datefmt='%H:%M:%S',
+                    level=logging.DEBUG)
+
+logger = logging.getLogger('dependency_log')
+logger.info("loading the dependency...")
 
 
 def my_ip():
+    logger.info("requesting my current ip to httpbin...")
     return requests.get("http://httpbin.org/ip").text
 
 
